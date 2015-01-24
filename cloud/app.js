6fa77a575c74d95c75cd1c24e0c8e41af008b4b0
Aditya Dev Gupta <adityadev93@gmail.com>
2015-01-24 00:16:01 -0800

    set up basic routing for all main pages

--- cloud/app.js
+++ cloud/app.js
@@ -1,5 +1,5 @@
 
-// These two lines are required to initialize Express in Cloud Code.
+// Initialize Express in Cloud Code.
 var express = require('express');
 var app = express();
 
@@ -11,10 +11,128 @@ app.use(express.bodyParser());    // Middleware for reading request body
 app.get('/', function(req, res) {
 
 	if (Parse.User.current()) {
-		res.render('pages/dashboard', {message: null, currentUser: Parse.User.current().getUsername() });
+
+		res.render('pages/dashboard', {
+			message: null, 
+			currentUser: Parse.User.current().getUsername(),
+			title: "Dashboard | inturn"
+		});
+
 	} else { 
 		// res.render('pages/index', {message: null, currentUser: null });
-		res.render('pages/start', {message:null});
+		res.render('pages/start', {
+			message: null,
+			title: "Welcome | inturn"
+		});
+	}
+
+});
+
+app.get('/dashboard', function(req, res) {
+
+	if (Parse.User.current()) {
+
+		res.render('pages/dashboard', {
+			currentUser: Parse.User.current().getUsername(),
+			title: "Dashboard | inturn"
+		});
+
+	} else { 
+		res.render('pages/start', {
+			message: null,
+			title: "Start | inturn"
+		});
+	}
+
+});
+
+/* JOB APPLICATIONS */
+app.get('/jobs', function(req, res) {
+
+	if (Parse.User.current()) {
+
+		res.render('pages/jobs', { 
+			currentUser: Parse.User.current().getUsername() ,
+			title: "Job Applications | inturn"
+		});
+
+	} else {
+		res.render('pages/start', {
+			message:null,
+			title: "Welcome | inturn"
+		});
+	}
+
+});
+
+app.get('/events', function(req, res) {
+
+	if (Parse.User.current()) {
+
+		res.render('pages/events', { 
+			currentUser: Parse.User.current().getUsername(),
+			title: "Events | inturn"
+		});
+
+	} else {
+		res.render('pages/start', {
+			message:null,
+			title: "Welcome | inturn"
+		});
+	}
+
+});
+
+app.get('/messages', function(req, res) {
+
+	if (Parse.User.current()) {
+
+		res.render('pages/messages',{ 
+			currentUser: Parse.User.current().getUsername(),
+			title: "Messages | inturn"
+		});
+
+	} else {
+		res.render('pages/start', {
+			message:null,
+			title: "Welcome | inturn"
+		});
+	}
+
+});
+
+app.get('/documents', function(req, res) {
+
+	if (Parse.User.current()) {
+
+		res.render('pages/documents',{ 
+			currentUser: Parse.User.current().getUsername(),
+			title: "Documents | inturn"
+		});
+
+	} else {
+		res.render('pages/start', {
+			message: null,
+			title: "Welcome | inturn"
+		});
+	}
+
+});
+
+app.get('/contacts', function(req, res) {
+
+	if (Parse.User.current()) {
+
+		res.render('pages/contacts', { 
+			currentUser: Parse.User.current().getUsername(),
+			title: "Contacts | inturn" 
+		});
+
+	} else {
+		res.render('pages/start', {
+			message:null,
+			title: "Contacts | inturn"
+		});
 	}
 
 });
@@ -22,14 +140,52 @@ app.get('/', function(req, res) {
 app.get('/start', function(req, res) {
 
   if (Parse.User.current()) {
-    res.render('pages/dashboard', {message: null, currentUser: Parse.User.current().getUsername() });
+
+    res.render('pages/dashboard', {
+    	message: null, 
+    	currentUser: Parse.User.current().getUsername(),
+    	title: "Welcome | inturn" 
+    });
+
   } else { 
     // res.render('pages/index', {message: null, currentUser: null });
-    res.render('pages/start', {message:null});
+    res.render('pages/start', {
+    	message:null,
+    	title: "Welcome | inturn"
+    });
   }
 
 });
 
+app.get('/login_post', function(req, res) {
+
+	if (Parse.User.current()) {
+
+    res.render('pages/dashboard', {
+    	message: null, 
+    	currentUser: Parse.User.current().getUsername(),
+    	title: "Welcome | inturn" 
+    });
+
+  } else { 
+    // res.render('pages/index', {message: null, currentUser: null });
+    res.render('pages/start', {
+    	message:null,
+    	title: "Welcome | inturn"
+    });
+  }
+
+});
+
+app.get('/logout', function(req, res) {
+
+	Parse.User.logOut();
+
+	res.redirect('/');
+
+});
+
+/* LOGIN */
 app.post('/login_post', function(req, res) {
 	username = req.body.username;
 	password = req.body.password;
@@ -41,8 +197,13 @@ app.post('/login_post', function(req, res) {
 
 		  	Parse.User.logIn(username, password, {
 				  success: function(user) {
-				  	res.render('pages/dashboard', {message: "User " + username + " succesfully created",
-				  		currentUser: Parse.User.current().getUsername()});
+
+				  	res.render('pages/dashboard', {
+				  		message: "User " + username + " succesfully created",
+				  		currentUser: Parse.User.current().getUsername(),
+				  		title: "Dashboard | inturn"
+				  	});
+
 				  },
 				  error: function(user, error) {
 				    
@@ -52,18 +213,27 @@ app.post('/login_post', function(req, res) {
 		  },
 		  error: function(user, error) {
 		  	errmsg = "User " + username + " already exists"
-		  	res.render('pages/start', {message: errmsg});
+		  	res.render('pages/start', {
+		  		message: errmsg,
+		  		title: "Welcome | inturn"
+		  	});
 		  }
 		});
 	} else { /* Existing user */
 
 		Parse.User.logIn(username, password, {
 		  success: function(user) {
-		  	res.render('pages/dashboard', {message: "Welcome back, " + username,
-		  	currentUser: Parse.User.current().getUsername()});
+		  	res.render('pages/dashboard', {
+		  		message: "Welcome back, " + username,
+		  		currentUser: Parse.User.current().getUsername(),
+		  		title: "Dashboard | inturn"
+		  	});
 		  },
 		  error: function(user, error) {
-		    res.render('pages/start', {message: "failed to log in"});
+		    res.render('pages/start', {
+		    	message: "failed to log in",
+		    	title: "Welcome | inturn"
+		    });
 		  }
 		});
 
@@ -71,15 +241,6 @@ app.post('/login_post', function(req, res) {
 
 });
 
-app.get('/dashboard', function(req, res) {
-
-	if (Parse.User.current()) {
-		res.render('pages/dashboard', {currentUser: Parse.User.current().getUsername()});
-	} else { 
-		res.render('pages/index', {currentUser: null, message: null});
-	}
-
-});
 
 // // Example reading from the request query string of an HTTP get request.
 // app.get('/test', function(req, res) {
