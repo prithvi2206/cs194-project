dc0a112b4f2e2e7f3ed3c27205eec62f9712b7b1
Ricky Tran <rtran58@DN51us37.SUNet>
2015-02-05 05:29:09 -0800

    added event handlers to sync active elements (between file selection and version selection)

--- public/js/inturn.js
+++ public/js/inturn.js
@@ -12,4 +12,22 @@ $(function() {
         });
     });
 
+    $('a.revision-point').click(function(e) {
+        var document_id = this.id.split("rev-point-")[1];
+        var data = {
+            document_id: document_id
+        };
+
+        $('.revision-history-animation-container div.active').removeClass('active');
+        $(this).find(".circle").addClass('active');
+
+        $('.list-group > a.active').removeClass('active');
+        $('.list-group > a#doc-'+document_id).addClass('active');
+
+        $.post("/documents/preview", data, function(data) {
+            $('#documentPreviewIFrame')[0].src = data.src
+        });
+
+        e.preventDefault();
+    });
 });
