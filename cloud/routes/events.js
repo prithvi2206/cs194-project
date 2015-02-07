128429f4912ef6cc933675da10949e46f7b10e54
Aditya Dev Gupta <adityadev93@gmail.com>
2015-02-06 22:07:57 -0800

    added messages and events to routes/controller decomp

--- cloud/routes/events.js
+++ cloud/routes/events.js
@@ -1,27 +1,8 @@
 'use strict';
 
-//var auth = require("../controllers/auth_controller.js");
+var events = require("../controllers/events_controller.js");
 
 module.exports = function(app) {
-	app.get('/events', function(req, res) {
-
-		if (Parse.User.current()) {
-
-			Parse.User.current().fetch()
-
-			res.render('pages/events', { 
-				currentUser: Parse.User.current().getUsername(),
-				title: "Events | inturn",
-				page: "events"
-			});
-
-		} else {
-			res.render('pages/start', {
-				message:null,
-				title: "Welcome | inturn"
-			});
-		}
-
-	});
+	app.get('/events', events.main);
 };
 
--- cloud/routes/messages.js
+++ cloud/routes/messages.js
@@ -1,26 +1,7 @@
 'use strict';
 
-//var auth = require("../controllers/auth_controller.js");
+var messages = require("../controllers/messages_controller.js");
 
 module.exports = function(app) {
-	app.get('/messages', function(req, res) {
-
-		if (Parse.User.current()) {
-
-			Parse.User.current().fetch()
-
-			res.render('pages/messages',{ 
-				currentUser: Parse.User.current().getUsername(),
-				title: "Messages | inturn",
-				page: "messages"
-			});
-
-		} else {
-			res.render('pages/start', {
-				message:null,
-				title: "Welcome | inturn"
-			});
-		}
-
-	});
+	app.get('/messages', messages.main);
 };
