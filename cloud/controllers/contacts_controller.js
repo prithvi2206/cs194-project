2878e0e96e11060f6acd331a352a7f5773425500
Prithvi Ramakrishnan <prithvir@stanford.edu>
2015-02-09 04:32:24 -0400

    added functionality for application to be added to contact

--- cloud/controllers/contacts_controller.js
+++ cloud/controllers/contacts_controller.js
@@ -25,9 +25,6 @@ exports.main = function(req, res) {
 	if (Parse.User.current()) {
 
 		Parse.User.current().fetch();
-
-		/* Contacts home */
-		// if(!req.params.op) {
 			
 		var ContactObj = Parse.Object.extend("Contact");
 		var query = new Parse.Query(ContactObj);
@@ -40,28 +37,34 @@ exports.main = function(req, res) {
 				console.log(error.message);
 			}
 		});
-
-		// }
-
-		/* Add contact form */
-		// else if (req.params.op == "add") {
-		// 	res.render('pages/contacts/add', { 
-		// 		currentUser: Parse.User.current().getUsername() ,
-		// 		title: "Add New Contact | inturn",
-		// 		page: "contacts"
-		// 	});
-		// }
-		// else {
-		// 	res.redirect('pages/contacts');
-		// }
-
-
 	} else {
 		res.render('pages/start', {
 			message:null,
 			title: "Welcome | inturn"
 		});
 	}
+}
+
+var addAppAndSend = function(res, contact_entry, id) {
+	var AppObj = Parse.Object.extend("Application");
+	var query = new Parse.Query(AppObj);
+	query.equalTo("objectId", id);
+	query.find({
+		success: function(results) {
+			console.log("found contact");
+			console.log("app is " + results[0]);
+			contact_entry.set("appId", results[0]);
+			contact_entry.save().then(function() { 
+				console.log("new contact saved succesfully");
+			}, function(error) {
+				console.log("new app did not save properly");
+			});
+			res.redirect('/contacts');
+		},
+		error: function(error) {
+			console.log(error.message);
+		}
+	});
 }
 
 exports.add = function(req, res) {
@@ -76,7 +79,6 @@ exports.add = function(req, res) {
 		var phone = req.body.phone;
 		var notes = req.body.notes;
 		var app = req.body.appselect;
-		console.log("app is" + app.id);
 
 		console.log("Going to add contact " + name + ", " + title + ", at " + company);
 
@@ -88,16 +90,10 @@ exports.add = function(req, res) {
 		contact_entry.set("title", title);
 		contact_entry.set("company", company);
 		contact_entry.set("email", email);
-		contact_entry.set("phone", email);
+		contact_entry.set("phone", phone);
 		contact_entry.set("notes", notes);
-		contact_entry.save().then(function() { 
-			console.log("new contact saved succesfully");
-		}, function(error) {
-			console.log("new app did not save properly");
-		});
-
-		res.redirect('/contacts');
-
+		console.log("calling function");
+		var appObject = addAppAndSend(res, contact_entry, app);
 	} else {
 		res.render('pages/start', {
 			message:null,
