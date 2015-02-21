f845d86056e4ffa48a77aff9ebfa78892e3c9395
Aditya Dev Gupta <adityadev93@gmail.com>
2015-02-20 22:44:52 -0800

    CSR stuff and app.js modifications

--- cloud/app.js
+++ cloud/app.js
@@ -22,11 +22,11 @@ app.use(express.static(__dirname + '/public'));
 
 /* At the top, with other redirect methods before other routes */
 app.get('*',function(req,res,next){
-  if(req.headers['x-forwarded-proto']!='https' && req.headers.host == "localhost")
+  if(req.headers['x-forwarded-proto']!='https')
     res.redirect('https://inturn.herokuapp.com'+req.url)
   else
     next() /* Continue to other routes if we're not redirecting */
-});
+});	
 
 var auth = require('./routes/auth')(app)
 	, dashboard = require('./routes/dashboard')(app)
