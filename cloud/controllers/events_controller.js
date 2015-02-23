68bc730905b71dd579bc4e22f1d44fe85a4c5838
Prithvi Ramakrishnan <prithvir@stanford.edu>
2015-02-23 01:33:04 -0800

    basic console-directed mail retrieval

--- cloud/controllers/events_controller.js
+++ cloud/controllers/events_controller.js
@@ -5,13 +5,32 @@ exports.main = function(req, res) {
 
 		Parse.User.current().fetch()
 
-		var Gmail = require('node-gmail-api')
-		  , gmail = new Gmail(key)
-		  , s = gmail.messages('label:inbox', {max: 10})
-		 
-		s.on('data', function (d) {
-		  console.log(d.snippet)
-		})
+		var token = Parse.User.current().get("google_token");
+
+		// var gcal = new Calendar(token);
+		// var gapi = require('node-gmail-api');
+		// gapi.auth.authorize(
+  //     {client_id: clientId, scope: scopes, immediate: false},
+  //     handleAuthResult);
+
+
+		// var resource = {
+		// 	"summary": "Appointment",
+		// 	"location": "Somewhere",
+		// 	"start": {
+		//  		"dateTime": "2015-02-23T10:00:00.000-07:00"
+		// 	},
+		// 	"end": {
+		// 		"dateTime": "2011-02-23T10:25:00.000-07:00"
+		// 	}
+		// };
+		// var request = gapi.client.calendar.events.insert({
+		// 	'calendarId': 'primary',
+		// 	'resource': resource
+		// });
+		// request.execute(function(resp) {
+		// 	console.log(resp);
+		// });
 
 		res.render('pages/events/main', { 
 			currentUser: Parse.User.current().getUsername(),
