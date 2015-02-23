85ed3f8ea3462a1e1b30061e5f93e8875d73f1e0
Aditya Dev Gupta <adityadev93@gmail.com>
2015-02-22 17:06:42 -0800

    forgot to add a bunch of files related to prev commit  oops

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '1073490943584-jihl83sesm1qcm10lik7mu86t5ioh5g5.apps.googleusercontent.com',
+        'clientSecret'  : 'MKAnbihZSzT75VOC61bapiPQ',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+
+};
