e106b80628618f19837a3f28a1f0753ac0e64cd3
Prithvi Ramakrishnan <prithvir@stanford.edu>
2015-02-28 07:25:01 -0800

    more merge conflicts

--- cloud/controllers/events_controller.js
+++ cloud/controllers/events_controller.js
@@ -2,71 +2,65 @@
 
 exports.main = function(req, res) {
 
-		Parse.User.current().fetch()
-		// var clientID = '1073490943584-jihl83sesm1qcm10lik7mu86t5ioh5g5.apps.googleusercontent.com';
-  //       var clientSecret = 'MKAnbihZSzT75VOC61bapiPQ';
-  //       var callbackURL ='http://localhost:3000/auth/google/callback';
-  //       var scope = 'profile email https://www.googleapis.com/auth/calendar https://mail.google.com/';
+	Parse.User.current().fetch()
+	// var clientID = '1073490943584-jihl83sesm1qcm10lik7mu86t5ioh5g5.apps.googleusercontent.com';
+//       var clientSecret = 'MKAnbihZSzT75VOC61bapiPQ';
+//       var callbackURL ='http://localhost:3000/auth/google/callback';
+//       var scope = 'profile email https://www.googleapis.com/auth/calendar https://mail.google.com/';
 
-		// var token = Parse.User.current().get("google_token");
-		// // var cal
-		// // var gcal = new Calendar(token);
-		// var gapi = require('googleapis');
-		// // gapi.auth.authorize(
-  // //     {client_id: clientID, scope: scope, immediate: false},
-  // //     handleAuthResult);	
-		// gapi.client.setApiKey();
+	// var token = Parse.User.current().get("google_token");
+	// // var cal
+	// // var gcal = new Calendar(token);
+	// var gapi = require('googleapis');
+	// // gapi.auth.authorize(
+// //     {client_id: clientID, scope: scope, immediate: false},
+// //     handleAuthResult);	
+	// gapi.client.setApiKey();
 
-		var google = require('googleapis');
-		// var urlshortener = google.urlshortener('v3');
-		var gcal = google.calendar('v3');
-		// var params = { shortUrl: 'http://goo.gl/xKbRu3' };
+	var google = require('googleapis');
+	// var urlshortener = google.urlshortener('v3');
+	var gcal = google.calendar('v3');
+	// var params = { shortUrl: 'http://goo.gl/xKbRu3' };
 
-		// get the long url of a shortened url
-		// urlshortener.url.get(params, function (err, response) {
-		//   console.log('Long url is', response.longUrl);
-		// });
-		var resource = {
-			"summary": "Appointment",
-			"location": "Somewhere",
-			"start": {
-		 		"dateTime": "2015-02-23T10:00:00.000-07:00"
-			},
-			"end": {
-				"dateTime": "2011-02-23T10:25:00.000-07:00"
-			}
-		};
-		var params = { calendarId: 'primary', resource:  resource};
+	// get the long url of a shortened url
+	// urlshortener.url.get(params, function (err, response) {
+	//   console.log('Long url is', response.longUrl);
+	// });
+	var resource = {
+		"summary": "Appointment",
+		"location": "Somewhere",
+		"start": {
+	 		"dateTime": "2015-02-23T10:00:00.000-07:00"
+		},
+		"end": {
+			"dateTime": "2011-02-23T10:25:00.000-07:00"
+		}
+	};
+	var params = { calendarId: 'primary', resource:  resource};
 
-		// gcal.events.insert(params, function(err, response) {
-		// 	console.log("done adding calendar element");
-		// });
-		
-		// gcal.calendarList.list({}, function(err, response) {
-		// 	var items = response.items;
-		// 	for (var i = 0; i < items.length; i++) {
-		// 		console.log(items[i].summary);
-		// 	};
-		// });
+	// gcal.events.insert(params, function(err, response) {
+	// 	console.log("done adding calendar element");
+	// });
+	
+	// gcal.calendarList.list({}, function(err, response) {
+	// 	var items = response.items;
+	// 	for (var i = 0; i < items.length; i++) {
+	// 		console.log(items[i].summary);
+	// 	};
+	// });
 
-		// var request = gapi.client.calendar.events.insert({
-		// 	'calendarId': 'primary',
-		// 	'resource': resource
-		// });
-		// request.execute(function(resp) {
-		// 	console.log(resp);
-		// });
+	// var request = gapi.client.calendar.events.insert({
+	// 	'calendarId': 'primary',
+	// 	'resource': resource
+	// });
+	// request.execute(function(resp) {
+	// 	console.log(resp);
+	// });
 
-		res.render('pages/events/main', { 
-			currentUser: Parse.User.current().getUsername(),
-			title: "Events | inturn",
-			page: "events"
-		});
+	res.render('pages/events/main', { 
+		currentUser: Parse.User.current().getUsername(),
+		title: "Events | inturn",
+		page: "events"
+	});
 
-	} else {
-		res.render('pages/start', {
-			message:null,
-			title: "Welcome | inturn"
-		});
-	}
 };
