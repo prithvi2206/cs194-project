bd4201cd8919a5f4f7722aa0e8eec2a1cd004f11
Aditya Dev Gupta <adityadev93@gmail.com>
2015-03-06 21:37:29 -0800

    fixed small bug with previous commit

--- public/js/messages.js
+++ public/js/messages.js
@@ -2,7 +2,6 @@ var renderMessage = function(name, email, subject, body) {
     var title = name + ' &lt;' + email + '&gt;';
     $('#messageTitle').html(title);
     $('#messageSubject').html(subject);
-    var body_parsed = jQuery('<div/>').append(body).find('body').html();
-    $('#messageBody').html(body_parsed);
+    $('#messageBody').html(body);
     $('#messageViewModal').modal('show');
 }
