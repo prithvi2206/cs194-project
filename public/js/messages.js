4b76446d498f7b60522835b14fb91eaacab592f4
Aditya Dev Gupta <adityadev93@gmail.com>
2015-03-06 16:59:29 -0800

    display body

--- public/js/messages.js
+++ public/js/messages.js
@@ -2,5 +2,6 @@ var renderMessage = function(data) {
     var title = data['senderName'] + ' &lt;' + data['senderEmail'] + '&gt;';
     $('#messageTitle').html(title);
     $('#messageSubject').html(data['subject']);
+    $('#messageBody').html(data['bodyHTML']);
     $('#messageViewModal').modal('show');
 }
