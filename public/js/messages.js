a68bfc9dc2a0b847aba4db0ac4b2a46d307084a7
Aditya Dev Gupta <adityadev93@gmail.com>
2015-03-06 21:36:17 -0800

    fixed escaping for bodyText not using BodyHTML for now

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,7 +1,8 @@
-var renderMessage = function(data) {
-    var title = data['senderName'] + ' &lt;' + data['senderEmail'] + '&gt;';
+var renderMessage = function(name, email, subject, body) {
+    var title = name + ' &lt;' + email + '&gt;';
     $('#messageTitle').html(title);
-    $('#messageSubject').html(data['subject']);
-    $('#messageBody').html(data['bodyHTML']);
+    $('#messageSubject').html(subject);
+    var body_parsed = jQuery('<div/>').append(body).find('body').html();
+    $('#messageBody').html(body_parsed);
     $('#messageViewModal').modal('show');
 }
