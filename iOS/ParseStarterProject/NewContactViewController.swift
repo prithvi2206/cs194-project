a005380b6183faebd2089dcd55efed9eb73d1fbd
Ricky Tran <rtran58@DN51usku.SUNet>
2015-03-08 16:20:35 -0700

    implemented auto fill based on contact selection

--- iOS/ParseStarterProject/NewContactViewController.swift
+++ iOS/ParseStarterProject/NewContactViewController.swift
@@ -17,7 +17,6 @@ class NewContactViewController: UIViewController, ABPeoplePickerNavigationContro
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var phoneNumberTextField: UITextField!
     @IBOutlet weak var companyTextField: UITextField!
-    @IBOutlet weak var notesTextFied: UITextField!
     @IBOutlet weak var notesTextField: UITextField!
     
     @IBAction func showContactPicker(sender: UIButton) {
@@ -30,8 +29,45 @@ class NewContactViewController: UIViewController, ABPeoplePickerNavigationContro
     }
     
     func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController!, didSelectPerson person: ABRecord!) {
-        if let name = ABRecordCopyValue(person, kABPersonFirstNameProperty).takeRetainedValue() as? String {
-            println(name)
+        if let first_name = ABRecordCopyValue(person, kABPersonFirstNameProperty)?.takeRetainedValue() as? String {
+            nameTextField.text = first_name + " "
+        }
+        
+        if let last_name = ABRecordCopyValue(person, kABPersonLastNameProperty)?.takeRetainedValue() as? String {
+            if(nameTextField.text == "") {
+                nameTextField.text = last_name
+            } else {
+                nameTextField.text = nameTextField.text + last_name
+            }
+        }
+        
+        if let company_name = ABRecordCopyValue(person, kABPersonOrganizationProperty)?.takeRetainedValue() as? String
+        {
+            companyTextField.text = company_name
+        }
+        
+        if let job_title = ABRecordCopyValue(person, kABPersonJobTitleProperty)?.takeRetainedValue() as? String {
+            titleTextField.text = job_title
+        }
+        
+        if let email: ABMultiValueRef = ABRecordCopyValue(person, kABPersonEmailProperty)?.takeRetainedValue(){
+            if (ABMultiValueGetCount(email) > 0) {
+                if let email = ABMultiValueCopyValueAtIndex(email, 0).takeRetainedValue() as? String {
+                    emailTextField.text = email
+                }
+            }
+        }
+        
+        if let phone: ABMultiValueRef = ABRecordCopyValue(person, kABPersonPhoneProperty)?.takeRetainedValue() {
+            if (ABMultiValueGetCount(phone) > 0) {
+                if let phone = ABMultiValueCopyValueAtIndex(phone, 0).takeRetainedValue() as? String {
+                    phoneNumberTextField.text = phone
+                }
+            }
+        }
+        
+        if let note = ABRecordCopyValue(person, kABPersonNoteProperty)?.takeRetainedValue() as? String {
+            notesTextField.text = note
         }
     }
     
