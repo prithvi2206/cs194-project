5a6fc7d8b222baed783283db74826abb1cf74291
Ricky Tran <rtran58@DN51us3j.SUNet>
2015-03-11 07:41:07 -0700

    converted previous new contact controller to a table view controller

---
+++ iOS/NewContactTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NewContactTableViewController.swift
+//  Inturn.io
+//
+//  Created by Ricky Tran on 3/11/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class NewContactTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
