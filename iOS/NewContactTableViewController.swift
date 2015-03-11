f8857c92e6988e2ecf46f0039e13d845673ea5a4
Ricky Tran <rtran58@DN51us3j.SUNet>
2015-03-11 09:00:17 -0700

    added job seletion table view

--- iOS/NewContactTableViewController.swift
+++ iOS/NewContactTableViewController.swift
@@ -7,9 +7,72 @@
 //
 
 import UIKit
+import AddressBook
+import AddressBookUI
 
-class NewContactTableViewController: UITableViewController {
+class NewContactTableViewController: UITableViewController, ABPeoplePickerNavigationControllerDelegate {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var companyTextField: UITextField!
+    @IBOutlet weak var notesTextField: UITextField!
+    
+    @IBAction func showContactPicker(sender: UIButton) {
+        var picker = ABPeoplePickerNavigationController()
+        picker.peoplePickerDelegate = self
+        
+        self.presentViewController(picker, animated: true) { () -> Void in
+        }
+    }
+    
+    func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController!, didSelectPerson person: ABRecord!) {
+        if let first_name = ABRecordCopyValue(person, kABPersonFirstNameProperty)?.takeRetainedValue() as? String {
+            nameTextField.text = first_name + " "
+        }
+        
+        if let last_name = ABRecordCopyValue(person, kABPersonLastNameProperty)?.takeRetainedValue() as? String {
+            if(nameTextField.text == "") {
+                nameTextField.text = last_name
+            } else {
+                nameTextField.text = nameTextField.text + last_name
+            }
+        }
+        
+        if let company_name = ABRecordCopyValue(person, kABPersonOrganizationProperty)?.takeRetainedValue() as? String
+        {
+            companyTextField.text = company_name
+        }
+        
+        if let job_title = ABRecordCopyValue(person, kABPersonJobTitleProperty)?.takeRetainedValue() as? String {
+            titleTextField.text = job_title
+        }
+        
+        if let email: ABMultiValueRef = ABRecordCopyValue(person, kABPersonEmailProperty)?.takeRetainedValue(){
+            if (ABMultiValueGetCount(email) > 0) {
+                if let email = ABMultiValueCopyValueAtIndex(email, 0).takeRetainedValue() as? String {
+                    emailTextField.text = email
+                }
+            }
+        }
+        
+        if let phone: ABMultiValueRef = ABRecordCopyValue(person, kABPersonPhoneProperty)?.takeRetainedValue() {
+            if (ABMultiValueGetCount(phone) > 0) {
+                if let phone = ABMultiValueCopyValueAtIndex(phone, 0).takeRetainedValue() as? String {
+                    phoneNumberTextField.text = phone
+                }
+            }
+        }
+        
+        if let note: CFStringRef = ABRecordCopyValue(person, kABPersonNoteProperty)?.takeRetainedValue() as? String {
+            
+            println(note)
+            
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
