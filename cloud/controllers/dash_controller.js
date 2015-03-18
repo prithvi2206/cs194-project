6ac1f3edc7f75e3d33f3c7503d4bacbad97785b3
Aditya Dev Gupta <adityadev93@gmail.com>
2015-03-18 05:56:27 -0700

    color codes

--- cloud/controllers/dash_controller.js
+++ cloud/controllers/dash_controller.js
@@ -4,28 +4,70 @@ var alerts = require("../util/alerts.js");
 var session = require("../util/session.js");
 var events = require("../util/events.js");
 
-exports.main = function(req, res) {
-	var token = Parse.User.current().get("google_token");
-	// refreshes the token and calls updateMessagesDB
+var main_get_unread = function(req, res, data) {
+	var MessageObj = Parse.Object.extend("Message");
+	var query = new Parse.Query(MessageObj);
+	query.equalTo("userId", Parse.User.current());
+	query.find({
+		success: function(results) {
 
+			/* find number of unread messages */
+			var num_unread = 0;
+			for (var i = 0; i < results.length; i++) {
+				if(results[i].get("flags").indexOf("UNREAD") > -1) {
+					num_unread++;
+				}
+			}
+			data["unread"] = num_unread;
+			res.render('pages/dashboard', { 
+				currentUser: Parse.User.current(),
+				title: "Dashboard | inturn",
+				page: "dashboard",
+				data: data,
+				message: null,
+				alerts: alerts.Alert
+			});
+
+		},
+		error: function(error) {
+			console.log(error.message);
+		}
+	});
+}
+
+var main_get_events = function(req, res, data) {
+
+	var EventObj = Parse.Object.extend("Event");
+	var query_event = new Parse.Query(EventObj);
+	query_event.equalTo("userId", Parse.User.current());
+	query_event.greaterThanOrEqualTo("start", new Date());
+	query_event.find({
+		success: function(results) {
+
+			data["events"] = results.length;
+			main_get_unread(req, res, data);
+		},
+		error: function(results) {
+			console.log(error.message);
+		}
+	});
+}
+
+exports.main = function(req, res) {
 
 	Parse.User.current().fetch()
 
 	var AppObj = Parse.Object.extend("Application");
 	var query = new Parse.Query(AppObj);
 	query.equalTo("userId", Parse.User.current());
+	query.equalTo("status", "not_applied");
 	query.find({
 		success: function(results) {
-			res.render('pages/dashboard', { 
-				currentUser: Parse.User.current(),
-				title: "Dashboard | inturn",
-				page: "dashboard",
-				jobs_count: results.length,
-				message: null,
-				alerts: alerts.Alert
-			});
 
-			alerts.reset();
+			var data = {};
+			data["jobs"] = results.length;
+
+			main_get_events(req, res, data);
 		},
 		error: function(error) {
 			console.log(error.message);
@@ -33,20 +75,6 @@ exports.main = function(req, res) {
 	});
 
 };
-
-// var escapeHtml = function(string) {
-// 	return String(string).replace(/[&<>"'\/]/g, function (s) {
-// 	  return entityMap[s];
-// 	});
-// }
-
-// var escapeAll = function(messages) {
-// 	var html = [];
-// 	for (var i = 0; i < messages.length; i++) {
-// 		html.push(escapeHtml(messages[i].get("bodyHTML")));
-// 	}
-// 	return html;
-// }
 
 exports.update_profile = function(req, res) {
 	var user = Parse.User.current();
